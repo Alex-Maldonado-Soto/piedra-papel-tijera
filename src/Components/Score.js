3c86f6aebb38485d1f5d8735afc41722eb0d3539
Alex-Maldonado <alexandermaldonadosoto@gmail.com>
2024-08-21 18:43:15 -0600

    build(All the proyect): :rocket: Use npm run build para desplegar el proyecto

--- src/Components/Score.js
+++ src/Components/Score.js
@@ -2,12 +2,7 @@ import React, { useState } from "react";
 import img from "../images/img";
 
 export const Score = ({ winner, score}) => {
-
-  if (!localStorage.getItem('score')) {
-    localStorage.setItem('score', 10);
-  }
   
-
   return (
     <header className="score">
       <figure className="score__figure">
